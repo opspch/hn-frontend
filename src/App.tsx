9a36cbb2b36f329213ac7fd105f016ee5bf2e60b
opspch <1962208-opspch@users.noreply.gitlab.com>
2025-11-05 20:47:20 +0100

    add react-query and fetch stories

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,38 @@ import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
+  const [page, setPage] = useState(1)
+  interface Story {
+    id: number;
+    title: string;
+    points: number;
+    user: string;
+    time: number;
+    time_ago: string;
+    comments_count: number;
+    type: string;
+    url: URL;
+    domain: URL
+  }
+  const fetchStories = (page: number): Promise<Story[]> => fetch('https://api.hackerwebapp.com/news?page=' + page).then(response => response.json())
+  const query = useQuery({
+    queryKey: ['stories', page],
+    queryFn: () => fetchStories(page),
+  })  
+  console.log(query);
+
 
   return (
     <>
       <div className='text-xl'>Hello world</div>
+      {query.isFetching ?
+        "Loading..." :
+        query.isError ?
+          query.error :
+          query.data?.map(item => <div>{item.title}</div>)}
     </>
   )
 }
