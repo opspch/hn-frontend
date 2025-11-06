ae8892d42f8d122c4d0ca9f66ad1eedeabe7d5a1
opspch <1962208-opspch@users.noreply.gitlab.com>
2025-11-06 16:55:41 +0100

    dont refetch queries on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import { QueryClient, QueryClientProvider }  from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
