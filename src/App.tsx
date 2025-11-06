9fca3a47daf0c0a2273d81ed842d9c9f78adea71
opspch <1962208-opspch@users.noreply.gitlab.com>
2025-11-06 16:22:51 +0100

    Story domain and url is a plain string, move Story type to top-level

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@ import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
+interface Story {
+  id: number;
+  title: string;
+  points: number;
+  user: string;
+  time: number;
+  time_ago: string;
+  comments_count: number;
+  type: string;
+  url: string;
+  domain: string;
+}
 
 function App() {
   const [count, setCount] = useState(0)
   const [page, setPage] = useState(1)
-  interface Story {
-    id: number;
-    title: string;
-    points: number;
-    user: string;
-    time: number;
-    time_ago: string;
-    comments_count: number;
-    type: string;
-    url: URL;
-    domain: URL
-  }
+  
   const fetchStories = (page: number): Promise<Story[]> => fetch('https://api.hackerwebapp.com/news?page=' + page).then(response => response.json())
   const query = useQuery({
     queryKey: ['stories', page],
@@ -33,7 +34,7 @@ function App() {
         "Loading..." :
         query.isError ?
           query.error :
-          query.data?.map(item => <div>{item.title}</div>)}
+          query.data?.map(item => <div key={item.id}>{item.title}</div>)}
     </>
   )
 }
