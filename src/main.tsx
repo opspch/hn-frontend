a2fee9b828a880946af6fbb2d211208958ee2af9
opspch <1962208-opspch@users.noreply.gitlab.com>
2025-11-08 22:16:31 +0100

    enable preloading on internal navigation via Link

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, defaultPreload: 'intent' })
 
 declare module '@tanstack/react-router' {
   interface Register {
