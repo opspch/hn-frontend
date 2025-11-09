785effb85e243ba71a3a839ae00a5491fa33e54c
opspch <1962208-opspch@users.noreply.gitlab.com>
2025-11-09 20:58:59 +0100

    smaller font size in about

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute('/about')({
 })
 
 function About() {
-  return <div className="p-2">
+  return <div className="p-2 text-base">
     This is built with React, Tailwind CSS, @tanstack/react-router and @tanstack/react-query. I started from the vite react-ts template.
     The Hacker News API I used is here: <a className='hover:underline visited:text-gray' href='https://github.com/cheeaun/node-hnapi'>https://github.com/cheeaun/node-hnapi</a>.
     The source code is here: <a className='hover:underline visited:text-gray' href='https://github.com/opspch/hn-frontend'>https://github.com/opspch/hn-frontend/</a>
