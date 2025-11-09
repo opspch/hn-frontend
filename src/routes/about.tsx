d7c55291ccda0348ea97d44f049146a13cf22b2b
opspch <1962208-opspch@users.noreply.gitlab.com>
2025-11-09 19:28:55 +0100

    add github link

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -8,6 +8,6 @@ function About() {
   return <div className="p-2">
     This is built with React, Tailwind CSS, @tanstack/react-router and @tanstack/react-query. I started from the vite react-ts template.
     The Hacker News API I used is here: <a className='hover:underline visited:text-gray' href='https://github.com/cheeaun/node-hnapi'>https://github.com/cheeaun/node-hnapi</a>.
-    The source code is here: <a className='hover:underline visited:text-gray' href=''>https://github.com/</a>
+    The source code is here: <a className='hover:underline visited:text-gray' href='https://github.com/opspch/hn-frontend'>https://github.com/opspch/hn-frontend/</a>
   </div>
 }
